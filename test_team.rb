ba4788fbc9739957e19e563869de5ee7159dc236
Lukeveille <luke@lukeleveille.com>
2018-10-31 15:07:39 -0400

    completed exercise

--- test_team.rb
+++ test_team.rb
@@ -4,4 +4,9 @@ require './team.rb'
 
 class TestTeam < MiniTest::Test
 
+  def test_to_hash()
+    my_team = Team.new('Bender', 10, 'Bender')
+    assert(my_team.to_hash() == {team_name: 'Bender', level: 10, points: 0})
+  end
+
 end
